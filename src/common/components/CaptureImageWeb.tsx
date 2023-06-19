3ab4a908c6facbad513ecf61f735560da787df9b
Jaibir Singh <contact2jaibir@gmail.com>
2023-06-19 19:56:17 +0530

    update to prv commit.

--- src/common/components/CaptureImageWeb.tsx
+++ src/common/components/CaptureImageWeb.tsx
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import React, { useState, useEffect, Component } from "react";//useEffect, 
+import React, { useState, useEffect, Component } from "react"; //useEffect,
 //import * as microsoftTeams from "@microsoft/teams-js";
-import { SdkError, media } from "@microsoft/teams-js";//, geoLocation
+import { SdkError, media } from "@microsoft/teams-js"; //, geoLocation
 import { Text, Button, Image, Card } from "@fluentui/react-components";
 //import CheckAndAlertForCameraPermission from "../helpers/NavigatorPermission";
 import { CardBody } from "reactstrap";
@@ -18,79 +18,80 @@ import TestAPIs from "../../common/constants/TestAPIs";
 const CaptureImageWeb: React.FC<iTabContext> = (props) => {
   //var imageCapture: ImageCapture;
   const [capturedImage, setCapturedImage] = useState("");
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
-  const [accuracy, setAccuracy] = useState('');
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [accuracy, setAccuracy] = useState("");
   const [isLocation, setIsLocation] = useState(false);
   const [isSendingCapt, setIsSendingCapt] = useState(false);
-  const [capImgUpRes, setCapImgUpRes] = useState('');
-  
+  const [capImgUpRes, setCapImgUpRes] = useState("");
+  const [imgBase64, setImgBase64] = useState("");
+
   const sendCaptureImage = () => {
     if (isSendingCapt) return;
     // update state
     // send the actual request
     setCapImgUpRes("Loading...");
-    if (!capturedImage) {
+    if (!imgBase64 || imgBase64 == '') {
       setCapImgUpRes("Failed! First capture photo.");
     } else {
       setIsSendingCapt(true);
       var formData = new FormData();
       formData.append("TeamId", props.teamId);
       formData.append("ChannelId", props.channelId);
-      formData.append("base64", capturedImage);
+      formData.append("base64", imgBase64);
       formData.append("CreatedBy", props.createdBy);
-      try{
-        
-      fetch(TestAPIs.UploadPhotoB64Url, {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${props.token}`,
-        },
-        body: formData,
-      })
-        .then((response) => response.json())
-        .then((resData) => {
-          console.log(resData);
-          setCapImgUpRes(JSON.stringify(resData));
-          setIsSendingCapt(false);
+      try {
+        fetch(TestAPIs.UploadPhotoB64Url, {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${props.token}`,
+            "content-type": ""
+          },
+          body: formData,
         })
-        .catch((err) => {
-          setCapImgUpRes(`Response Error: ${err.message}`);
-          setIsSendingCapt(false);
-        });
-      }
-      catch(err : any){
+          .then((response) => response.json())
+          .then((resData) => {
+            console.log(resData);
+            setCapImgUpRes(JSON.stringify(resData));
+            setIsSendingCapt(false);
+          })
+          .catch((err) => {
+            setCapImgUpRes(`Response Error: ${err.message}`);
+            setIsSendingCapt(false);
+          });
+      } catch (err: any) {
         setCapImgUpRes(`Response Error: ${err.message}`);
       }
     }
   };
 
-  const getCurrentLocation =()=> {        
-      navigator.permissions.query({ name: 'geolocation' }).then(function (result) {
-          if (result.state === 'denied') {
-              //setShowComments(true);
-              //return false;
-              setIsLocation(false);
-          }
-          else {
-              //setShowComments(false);
-              setIsLocation(true);
-          }
+  const getCurrentLocation = () => {
+    navigator.permissions
+      .query({ name: "geolocation" })
+      .then(function (result) {
+        if (result.state === "denied") {
+          //setShowComments(true);
+          //return false;
+          setIsLocation(false);
+        } else {
+          //setShowComments(false);
+          setIsLocation(true);
+        }
       });
-      
-      // Method to ask for image capture permission and then select media
-      navigator.geolocation.getCurrentPosition(function (position) {
-          var coordinates = position.coords;
-          // var location = {
-          //     latitude: coordinates.latitude,
-          //     longitude: coordinates.longitude,
-          // }
-          //setGeoLocationValue(JSON.stringify(location))
-          setLatitude(coordinates.latitude.toString());
-          setLongitude(coordinates.longitude.toString());
-          setAccuracy(coordinates.accuracy.toString());
-      });
-  }
+
+    // Method to ask for image capture permission and then select media
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var coordinates = position.coords;
+      // var location = {
+      //     latitude: coordinates.latitude,
+      //     longitude: coordinates.longitude,
+      // }
+      //setGeoLocationValue(JSON.stringify(location))
+      setLatitude(coordinates.latitude.toString());
+      setLongitude(coordinates.longitude.toString());
+      setAccuracy(coordinates.accuracy.toString());
+    });
+  };
 
   useEffect(() => {
     getCurrentLocation();
@@ -130,6 +131,7 @@ const CaptureImageWeb: React.FC<iTabContext> = (props) => {
           // img.src = "data:" + y.mimeType + ";base64," + y.preview;
           let imageString = y.mimeType + ";base64," + y.preview;
           setCapturedImage(imageString);
+          setImgBase64(y.preview);
         }
         return true;
       }
@@ -141,8 +143,8 @@ const CaptureImageWeb: React.FC<iTabContext> = (props) => {
     // navigator.geolocation.getCurrentPosition(success, error, options);
   };
 
-    // Method to validate before capturing media
-  
+  // Method to validate before capturing media
+
   return (
     <>
       {/* Card for capturing single image */}
@@ -163,18 +165,17 @@ const CaptureImageWeb: React.FC<iTabContext> = (props) => {
             <Text>navigator, microsoftTeams </Text><br/>
             <Text weight="medium">Method: </Text><br/>
             <Text>navigator.mediaDevices.getUserMedia, teams.getmedia</Text><br/> */}
-            <Button onClick={deviceCapabilities}>Capture image</Button><br/>
+            <Button onClick={deviceCapabilities}>Capture image</Button>
+            <br />
             <Image src={capturedImage} height={100} />
-            {
-              capturedImage && capturedImage.length > 10 ? (
-                <Button appearance="primary" onClick={sendCaptureImage}>
-                  Upload Capture Image
-                </Button>
-                ) : ( <></> )
-            }
-            <div>
-              {capImgUpRes}
-            </div>
+            {capturedImage && capturedImage.length > 10 ? (
+              <Button appearance="primary" onClick={sendCaptureImage}>
+                Upload Capture Image
+              </Button>
+            ) : (
+              <></>
+            )}
+            <div>{capImgUpRes}</div>
           </div>
         </CardBody>
       </Card>
